ff4cefeef5d269b958977a0ca3aae3dbda348f21
Kevin Colten <kevincolten@gmail.com>
2020-11-17 15:44:31 -0600

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if($this->app->environment() === 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
