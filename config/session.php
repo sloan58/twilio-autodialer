1f4e3aaafabffac2ce7748df50f9061e50085600
Kevin Colten <kevincolten@gmail.com>
2020-11-23 10:23:23 -0600

    same_site

--- config/session.php
+++ config/session.php
@@ -176,4 +176,19 @@ return [
 
     'http_only' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Same-Site Cookies
+    |--------------------------------------------------------------------------
+    |
+    | This option determines how your cookies behave when cross-site requests
+    | take place, and can be used to mitigate CSRF attacks. By default, we
+    | will set this value to "lax" since this is a secure default value.
+    |
+    | Supported: "lax", "strict", "none", null
+    |
+    */
+
+    'same_site' => 'none',
+
 ];
